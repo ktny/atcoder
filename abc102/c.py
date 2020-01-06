d78ff7b0c59404fda19717244e8b6395007a9926
katsunori.t10@gmail.com <katsunori.t10@gmail.com>
2020-01-06 23:28:08 +0900

    add ABC102

--- abc102/c.py
+++ abc102/c.py
@@ -1,14 +1,5 @@
 N = int(input())
 A = list(map(int, input().split()))
-
-minv = min(A)
-maxv = max(A)
-ans = (-1, float('inf'))
-for b in range(maxv+1):
-    t = 0
-    for i in range(N):
-        t += abs(A[i] - (b+i+1))
-    print(b, t)
-    if ans[1] > t:
-        ans = (b, t)
-print(ans)
+B = sorted([A[i]-(i+1) for i in range(N)])
+b = B[N//2]
+print(sum([abs(B[i]-b) for i in range(N)]))
