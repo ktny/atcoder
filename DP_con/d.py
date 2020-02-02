8b4c5e4d39b223b9f1ec05f244bd4facc676dd61
katsunori.t10@gmail.com <katsunori.t10@gmail.com>
2020-02-02 16:49:21 +0900

    modify DP d

--- DP_con/d.py
+++ DP_con/d.py
@@ -1,3 +1,14 @@
 N, W = map(int, input().split())
-wv = [list(map(int, input().split())) for i in range(N)]
+wv = sorted([list(map(int, input().split())) for i in range(N)], key=lambda x: x[0])
 
+# 重さをi、dp[i]を価値の最大値とする
+dp = [0]*(W+1)
+dp[0] = 0
+
+for i in range(N):
+    w, v = wv[i]
+    # 前から埋めていくとさっき更新したやつをもとに更新してしまう（同じものを使ってしまう）ので逆から更新する
+    for j in range(W, w-1, -1):
+        dp[j] = max(dp[j], dp[j-w] + v)
+
+print(dp[W])
