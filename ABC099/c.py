c603d196fbd6a42a8ed8e9e59eab8f8fb0e95a2b
katsunori.t10@gmail.com <katsunori.t10@gmail.com>
2020-02-23 11:28:55 +0900

    add ABC099

--- ABC099/c.py
+++ ABC099/c.py
@@ -1,34 +1,36 @@
+from math import log
 N = int(input())
-
-nums = []
-i = 1
+dp = [0] * (10**5+1)
+dpl = set()
+a = 1
+b = 1
 while True:
-    a = 6**i
-    b = 9**i
-    if a <= 100000:
-        nums.append(6**i)
+    if a > 10**5:
+        break
+    a *= 6
+    dpl.add(a)
+while True:
+    if b > 10**5:
+        break
+    b *= 9
+    dpl.add(b)
+
+for i in range(1, 10**5+1):
+    if i < 6:
+        dp[i] = i
+        continue
+    log6 = log(i, 6)
+    log9 = log(i, 9)
+    l = set(map(float, range(1, 20)))
+    # 6の乗数、9の乗数の場合は1でdpを埋める
+    if log6 in l:
+        dp[i] = 1
+    elif log9 in l:
+        dp[i] = 1
+    # それ以外の数値は各6, 9の乗数を引いた値から最も小さい数で埋める
     else:
-        break    
-    if b <= 100000:
-        nums.append(9**i)
-    i += 1
-nums.append(1)
-nums.sort(reverse=True)
-print(nums)
+        minl = [dp[i-d]+1 for d in dpl if i-d >= 0]
+        if minl:
+            dp[i] = min(minl)
 
-ans = 0
-while True:
-    print(N)
-    if N == 0:
-        print(ans)
-        exit()
-    for n in nums:
-        if N >= n:
-            N -= n
-            ans += 1
-            break
-
-dp = []
-48 = 6**2 + 6*2
-48 / 6 = 8
-8 % 6 = 2
+print(dp[N])
